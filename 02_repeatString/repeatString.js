3e4beef57c84a89314484d5c90ff160d24970fd8
Austricks <122561836+Austricks@users.noreply.github.com>
2024-05-22 15:45:08 +0200

    Create for loop to repeat string and the to RETURN the result

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let rptString = "";
+    for (i = 0; i < num; i++) {
+        rptString += string
+    }
+    return rptString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
