2a0da612286ed1b6978725973d9b3866cc473121
Austricks <122561836+Austricks@users.noreply.github.com>
2024-05-22 21:55:16 +0200

    Add if/else to return ERROR if num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(string, num) {
-    let rptString = "";
-    for (i = 0; i < num; i++) {
-        rptString += string
+    if (num <0) {
+        return "ERROR";
     }
-    return rptString;
+    else {
+        let rptString = "";
+        for (i = 0; i < num; i++) {
+            rptString += string;
+        }
+        return rptString;
+    };
 };
 // Do not edit below this line
 module.exports = repeatString;
