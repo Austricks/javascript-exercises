8baf4681502e546f6e08cb1a3111f326cd281ab3
Austricks <122561836+Austricks@users.noreply.github.com>
2024-05-23 17:44:52 +0200

    Return the reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ const reverseString = function(input) {
     let inputToArray = input.split('');
     let reversedArray = inputToArray.reverse();
     let arrayToString = reversedArray.join('')
-    
+    return arrayToString;
 };
 
 // Do not edit below this line
